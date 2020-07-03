da40d723460e12aab5360d1ea8cc1e4bfdbd87e8
Udalov Max <re.udalov@gmail.com>
2020-07-03 19:15:50 +0300

    week_3: names add solution 3

--- white_belt/week_3/names/person.cpp
+++ white_belt/week_3/names/person.cpp
@@ -1,9 +1,16 @@
+#include <algorithm>
 #include <map>
 #include <string>
 #include <vector>
 
 class Person {
 public:
+  Person(const std::string &first_name, const std::string &last_name,
+         int year) {
+    first_names[year] = first_name;
+    last_names[year] = last_name;
+  }
+
   void ChangeFirstName(int year, const std::string &first_name) {
     first_names[year] = first_name;
   }
@@ -12,12 +19,12 @@ public:
     last_names[year] = last_name;
   }
 
-  std::string GetFullName(int year) {
+  const std::string GetFullName(int year) const {
     auto first_name = get_first_name(year);
     auto last_name = get_last_name(year);
 
     if (first_name.empty() && last_name.empty())
-      return "Incognito";
+      return "No person";
 
     if (first_name.empty())
       return last_name + " with unknown first name";
@@ -28,12 +35,12 @@ public:
     return first_name + " " + last_name;
   }
 
-  std::string GetFullNameWithHistory(int year) {
+  const std::string GetFullNameWithHistory(int year) const {
     auto first_name = get_full_first_name(year);
     auto last_name = get_full_last_name(year);
 
     if (first_name.empty() && last_name.empty())
-      return "Incognito";
+      return "No person";
 
     if (first_name.empty()) {
       return last_name + " with unknown first name";
@@ -50,24 +57,24 @@ private:
   std::map<int, std::string> first_names;
   std::map<int, std::string> last_names;
 
-  const std::string get_first_name(int year) {
+  const std::string get_first_name(int year) const {
     return find_by_year(year, first_names);
   }
 
-  const std::string get_last_name(int year) {
+  const std::string get_last_name(int year) const {
     return find_by_year(year, last_names);
   }
 
-  const std::string get_full_first_name(int year) {
+  const std::string get_full_first_name(int year) const {
     return history_name(year, first_names);
   }
 
-  const std::string get_full_last_name(int year) {
+  const std::string get_full_last_name(int year) const {
     return history_name(year, last_names);
   }
 
-  const std::string find_by_year(int year,
-                                 const std::map<int, std::string> &names) {
+  const std::string
+  find_by_year(int year, const std::map<int, std::string> &names) const {
     for (auto iter = names.rbegin(); iter != names.rend(); ++iter) {
       if (iter->first <= year)
         return iter->second;
@@ -75,8 +82,8 @@ private:
     return "";
   }
 
-  const std::string history_name(int year,
-                                 const std::map<int, std::string> &names) {
+  const std::string
+  history_name(int year, const std::map<int, std::string> &names) const {
     std::vector<std::string> found_names;
 
     for (auto iter = names.rbegin(); iter != names.rend(); ++iter) {
